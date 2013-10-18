442eab1738589d95b6ed038f79fcdec6987cd056
ruttenberg <ruttenberg@stanfordalumni.org>
2013-10-18 13:52:26 -0400

    logic of Application process is here

---
+++ The-Single-Responsibility-Principle/java/src/main/java/com/theladders/solid/srp/ApplicationProcess.java
@@ -0,0 +1,11 @@
+package com.theladders.solid.srp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jpr
+ * Date: 10/18/13
+ * Time: 1:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ApplicationProcess {
+}
