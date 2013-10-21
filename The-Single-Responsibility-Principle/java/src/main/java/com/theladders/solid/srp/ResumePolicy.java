620abcb6e573c69224857f20931866a77e3eb064
ruttenberg <ruttenberg@stanfordalumni.org>
2013-10-21 15:17:21 -0400

    move resume processing into ResumePolicy from ApplicationProcess

---
+++ The-Single-Responsibility-Principle/java/src/main/java/com/theladders/solid/srp/ResumePolicy.java
@@ -0,0 +1,12 @@
+package com.theladders.solid.srp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jpr
+ * Date: 10/21/13
+ * Time: 2:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ResumePolicy
+{
+}
