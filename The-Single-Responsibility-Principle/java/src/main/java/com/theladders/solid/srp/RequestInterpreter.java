143293a45f1b3d611835aee8568db5ad9d58a9df
ruttenberg <ruttenberg@stanfordalumni.org>
2013-10-21 10:16:00 -0400

    move interpretation of Request data out of ApplyController into RequestInterpreter

---
+++ The-Single-Responsibility-Principle/java/src/main/java/com/theladders/solid/srp/RequestInterpreter.java
@@ -0,0 +1,11 @@
+package com.theladders.solid.srp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jpr
+ * Date: 10/21/13
+ * Time: 9:55 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RequestInterpreter {
+}
